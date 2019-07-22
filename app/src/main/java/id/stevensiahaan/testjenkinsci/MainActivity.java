617cdfd9e69ad89c07a6af65dd039d0e62411e56
stevensiahaan <stevenjhsiahaan@gmail.com>
2019-07-22 10:47:50 +0700

    test update webhook

--- app/src/main/java/id/stevensiahaan/testjenkinsci/MainActivity.java
+++ app/src/main/java/id/stevensiahaan/testjenkinsci/MainActivity.java
@@ -1,13 +1,18 @@
 package id.stevensiahaan.testjenkinsci;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHello = (TextView) findViewById(R.id.txtHello);
     }
 }
